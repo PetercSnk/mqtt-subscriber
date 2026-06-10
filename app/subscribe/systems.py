3de7205634aa871f59b9c4cea518ed1f5d76c4b7
petercsnk <peter.ch.snook@gmail.com>
2026-06-10 14:27:11 +0100

    added on & off functions for physical system

--- app/subscribe/systems.py
+++ app/subscribe/systems.py
@@ -1,10 +1,52 @@
+import time 
+
+import RPi.GPIO as GPIO
+
+
+# Setup for Raspberry Pi
+relay = 18
+switch = 12
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(relay, GPIO.OUT)
+GPIO.setup(switch, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
 def on():
-    """Code for turning physical system on goes below.
+    """Turns physical system on.
     """
-    print("on")
+    high = False
+    low = False
+    GPIO.output(relay, True)
+    while True:
+        i = GPIO.input(switch)
+        if (i == 0):
+            low = True
+            time.sleep(0.1)
+        elif (i == 1):
+            high = True
+            time.sleep(0.1)
+        if high and not low:
+            high = False
+        elif high and low:
+            GPIO.output(relay, False)
+            return
 
 
 def off():
-    """Code for turning physical system off goes below.
+    """Turns physical system off.
     """
-    print("off")
+    high = False
+    low = False
+    GPIO.output(relay, True)
+    while True:
+        i = GPIO.input(switch)
+        if (i == 0):
+            low = True
+            time.sleep(0.1)
+        elif (i == 1):
+            high = True
+            time.sleep(0.1)
+        if low and not high:
+            low = False
+        elif high and low:
+            GPIO.output(relay, False)
+            return
